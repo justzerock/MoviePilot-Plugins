7155bc1f1eb908c4206db5547b7d802015c7579b
justzerock <liu.zerock@gmail.com>
2024-07-18 01:07:28 +0800

    历史样式

--- plugins/doubanrankvote/__init__.py
+++ plugins/doubanrankvote/__init__.py
@@ -30,7 +30,7 @@ class DoubanRankVote(_PluginBase):
     # 插件图标
     plugin_icon = "movie.jpg"
     # 插件版本
-    plugin_version = "0.0.1"
+    plugin_version = "0.0.2"
     # 插件作者
     plugin_author = "justzerock"
     # 作者主页
