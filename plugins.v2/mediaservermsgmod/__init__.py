540107d1912c59343a67d73f88eab83453d95d4a
justzerock <liu.zerock@gmail.com>
2025-01-25 14:57:53 +0800

    精简通知详情

--- plugins.v2/mediaservermsgmod/__init__.py
+++ plugins.v2/mediaservermsgmod/__init__.py
@@ -18,7 +18,7 @@ class MediaServerMsgMod(_PluginBase):
     # 插件图标
     plugin_icon = "mediaplay.png"
     # 插件版本
-    plugin_version = "0.1"
+    plugin_version = "0.2"
     # 插件作者
     plugin_author = "jxxghp, justzerock"
     # 作者主页
